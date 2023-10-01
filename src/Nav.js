6dae1c852c4a853ce39eb29a19c8afdd870533be
Hafid Eledath <hafideledath@gmail.com>
2023-10-01 07:57:12 +0400

    Initial commit

--- src/Nav.js
+++ src/Nav.js
@@ -3,7 +3,7 @@ import './Nav.css';
 function Nav() {
   return (
     <nav>
-        <h6>DI@Tech'24</h6>
+        <a href="#"><h6>DI@Tech'24</h6></a>
         <div className="nav-links">
           <a href="#">Meet the team</a>
           <a href="#">Event details</a>
