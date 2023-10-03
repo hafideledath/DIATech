49a70e59bf6334fd6e8159ca32d8565bff21c946
Hafid Eledath <hafideledath@gmail.com>
2023-10-03 18:04:26 +0400

    added subtle gradient

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <div className="App">
-      <svg viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg"><defs><radialGradient id="c" r="100%" cx="34%" cy="70%"><stop offset="0%" stop-color="#da7eb7"/><stop offset="100%" stop-color="#b3a0ef"/></radialGradient><clipPath id="b"><path fill="currentColor" d="M813.5 664.5Q763 829 596 819.5t-352.5-58Q58 713 116 528t181-282q123-97 300.5-94t222 175.5q44.5 172.5-6 337Z"/></clipPath><filter id="a" x="-50vw" y="-50vh" width="100vw" height="100vh"><feFlood flood-color="#fff" result="neutral-gray"/><feTurbulence type="fractalNoise" baseFrequency="2.5" numOctaves="100" stitchTiles="stitch" result="noise"/><feColorMatrix in="noise" type="saturate" values="0" result="destaturatedNoise"/><feComponentTransfer in="desaturatedNoise" result="theNoise"><feFuncA type="table" tableValues="0 0 0.4 0"/></feComponentTransfer><feBlend in="SourceGraphic" in2="theNoise" mode="soft-light" result="noisy-image"/></filter></defs><g filter="url(#a)" clip-path="url(#b)"><path fill="url(#c)" d="M813.5 664.5Q763 829 596 819.5t-352.5-58Q58 713 116 528t181-282q123-97 300.5-94t222 175.5q44.5 172.5-6 337Z"/></g></svg>
+      <svg />
       <div className="hero">
         <Nav />
         <h1>DIA@Tech<span className="separate"><span style={{fontWeight: 400, whiteSpace: "pre"}}> is </span>back.</span></h1>
