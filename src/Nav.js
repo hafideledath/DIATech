f12b50588479b29ff3347323ccf474d6d10c677c
Hafid Eledath <hafideledath@gmail.com>
2023-10-11 15:47:46 +0400

    lol

--- src/Nav.js
+++ src/Nav.js
@@ -3,7 +3,7 @@ import './Nav.css';
 function Nav() {
   return (
     <nav>
-        <a href="#"><h6>DI@Tech'24</h6></a>
+        <a href="#"><h6>DI@Tech'<span className="year">24</span></h6></a>
         <div className="nav-links">
           <a href="#">Mission</a>
           <a href="#">Event details</a>
