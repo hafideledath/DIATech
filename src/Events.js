d2d9bc0c19f4d122914ae99cd6e47be8318ef43e
Hafid Eledath <hafideledath@gmail.com>
2023-11-08 10:43:10 +0400

    lol

--- src/Events.js
+++ src/Events.js
@@ -1,3 +1,7 @@
+import mBotRobotics from './events/mbot_robotics.webp';
+import primaryRobotics from './events/primary_robotics.webp';
+import hourOfCode from './events/hour_of_code.webp';
+
 import './Events.css';
 
 function Events() {
@@ -5,46 +9,46 @@ function Events() {
     <div className="Events">
         <h2 style={{marginTop: '120px'}}>Events</h2>
         <div className="calendar">
+          <h2>October</h2>
           <div className="event">
-            <h2>October</h2>
             <div className="event-details">
-              <img src="https://placehold.co/200x300" alt="placeholder" />
               <caption>mBot Robotics Workshop</caption>
+              <img src={mBotRobotics} alt="mBot Robotics" />
             </div>
           </div>
+          <h2>November</h2>
           <div className="event">
-            <h2>November</h2>
             <div className="event-details">
-              <img src="https://placehold.co/200x300" alt="placeholder" />
-              <caption>Robotics University Visit</caption>
+              <caption>Primary School Assembly</caption>
+              <img src={primaryRobotics} alt="placeholder" />
             </div>
           </div>
+          <h2>January</h2>
           <div className="event">
-            <h2>January</h2>
             <div className="event-details">
-              <img src="https://placehold.co/200x300" alt="placeholder" />
               <caption>DI@BYTE</caption>
+              <img src="https://placehold.co/200x300" alt="placeholder" />
             </div>
           </div>
+          <h2>February</h2>
           <div className="event">
-            <h2>February</h2>
             <div className="event-details">
-              <img src="https://placehold.co/200x300" alt="placeholder" />
               <caption>Hour of Code</caption>
+              <img src={hourOfCode} alt="placeholder" />
             </div>
           </div>
+          <h2>April</h2>
           <div className="event">
-            <h2>April</h2>
             <div className="event-details">
-              <img src="https://placehold.co/200x300" alt="placeholder" />
               <caption>Innovents Robotics</caption>
+              <img src="https://placehold.co/200x300" alt="placeholder" />
             </div>
           </div>
         </div>
+        <h2>June</h2>
         <div className="final event">
-            <h2>June</h2>
+          <caption>DI@TECH</caption>
             <img src="https://placehold.co/300x100" alt="placeholder" />
-            <caption>DI@TECH</caption>
         </div>
     </div>
   );
